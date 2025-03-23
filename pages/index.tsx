a76c49fe93c002b98c59c8b7dd20117ecdea4d24
langxie-web <yianrita@gmail.com>
2025-03-23 22:43:51 +0000

    added text decor" gi

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,14 +11,16 @@ export default function ProductListView({
       </h1>
       <ul className="px-8 mx-auto max-w-md sm:flex sm:gap-x-16 sm:justify-start sm:gap-y-8 sm:max-w-xl md:max-w-5xl">
         {products.map((p) => (
-          <li className="mb-6 shadow rounded-2xl flex flex-col content-between bg-white px-2 py-4 sm:basis-1/2 md:basis-1/3 md:max-h-[300px] md:justify-center md:text-center">
+          <li className="mb-6 shadow group hover:shadow-xl focus:shadow-xl rounded-2xl flex flex-col content-between bg-white px-2 py-4 sm:basis-1/2 md:basis-1/3 md:max-h-[300px] md:justify-center md:text-center">
             <img
               src={p.imageURL}
               alt={p.title}
               className="object-scale-down basis-2/3 mb-1 h-3/5 max-h-[320px] rounded-md mx-auto max-h-vwh"
             ></img>
-            <h4 className="font-semibold text-gray-500">
-              <div>{p.title}</div>
+            <h4 className="font-semibold text-gray-500 ">
+              <div className="group-hover:underline group-hover:decoration-wavy group-focus:underline group-focus:decoration-wavy">
+                {p.title}
+              </div>
               <div className="text-blue-700 font-bold font-serif text-sm">{`£${p.price}`}</div>
             </h4>
             <p className="text-gray-700 tracking-tighter mt-auto">
